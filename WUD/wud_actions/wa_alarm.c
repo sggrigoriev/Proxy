ab8d07013d23424df2f8bf2b9f69f9c341eebe13
sggrigoriev <sggrigoriev@gmail.com>
2017-02-23 22:10:08 +0300

    c90 removed

--- WUD/wud_actions/wa_alarm.c
+++ WUD/wud_actions/wa_alarm.c
@@ -11,6 +11,7 @@ static pthread_mutex_t local_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static lib_timer_clock_t ALARMS[PR_CHILD_SIZE] = {{0}};
 
+
 void wa_alarms_init() {
     unsigned int i;
     for(i = 0; i < PR_CHILD_SIZE; i++) wa_alarm_reset((pr_child_t)i);
