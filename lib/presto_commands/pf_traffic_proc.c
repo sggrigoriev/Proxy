56a0bca0553c88419fc7be540b572ae379d958e5
mlevitin <mlevitin@technoserv.com>
2017-11-21 15:49:36 +0300

    depr_pf_add_proxy_head - buffer issue mlevitin

--- lib/presto_commands/pf_traffic_proc.c
+++ lib/presto_commands/pf_traffic_proc.c
@@ -26,6 +26,7 @@
 #include "lib_http.h"
 
 #include "pf_traffic_proc.h"
+#include "pu_logger.h"
 
 /************************************************************************************************
 
@@ -51,8 +52,30 @@ static unsigned long inc_seq_number() {
  */
 size_t pf_add_proxy_head(char* msg, size_t msg_size, const char* device_id) {
     unsigned long number = inc_seq_number();
+    char buf[LIB_HTTP_MAX_MSG_SIZE*2];
 
+    memset(buf,0,sizeof(buf));
+    pu_log(LL_DEBUG,"%s: ===== 1 got message: '%s'",__FUNCTION__,msg);
+    char *p = strchr(msg,'{');
+    if (p) { /* message contains something starting with '{' */
+        p++;
+        sprintf(buf, "{\"proxyId\": \"%s\", \"sequenceNumber\": \"%lu\", %s\0", device_id, number, p);
+    }
+    else  /* empty message */
+        sprintf(msg, "{\"proxyId\": \"%s\", \"sequenceNumber\": \"%lu\"}\0", device_id, number);
+
+    strcpy(msg, buf);
+    msg[msg_size-1] = '\0';
+    pu_log(LL_ERROR,"%s:===== 5 resulting message: '%s'",__FUNCTION__,msg);
+    return strlen(msg);
+}
+
+size_t depr_pf_add_proxy_head(char* msg, size_t msg_size, const char* device_id) {
+    unsigned long number = inc_seq_number();
+
+    pu_log(LL_DEBUG,"%s: ===== 1 got message: '%s'",__FUNCTION__,msg);
     if(!strlen(msg)) {
+        pu_log(LL_ERROR,"%s: =====  2 ZERO_LENGTH message.",__FUNCTION__);
         snprintf(msg, msg_size-1, "{\"proxyId\": \"%s\", \"sequenceNumber\": \"%lu\"}", device_id, number);
     }
     else {
@@ -62,15 +85,17 @@ size_t pf_add_proxy_head(char* msg, size_t msg_size, const char* device_id) {
         while((msg[i++] != '{') && (i < strlen(msg)));
 
         if(i >= strlen(msg)) {
+            pu_log(LL_DEBUG,"%s: ===== 3 i=%d",__FUNCTION__,i);
             snprintf(buf, sizeof(buf) - 1, "{\"proxyId\": \"%s\", \"sequenceNumber\": \"%lu\"}", device_id, number);
         }
         else {
+            pu_log(LL_DEBUG,"%s: ===== 4 i=%d  ",__FUNCTION__,i);
             snprintf(buf, sizeof(buf)-1, "{\"proxyId\": \"%s\", \"sequenceNumber\": \"%lu\", %s", device_id, number, msg+1);
         }
 
         strncpy(msg, buf, msg_size-1);
         msg[msg_size-1] = '\0';
     }
-
+    pu_log(LL_ERROR,"%s:===== 5 resulting message: %'s'",__FUNCTION__,msg);
     return strlen(msg);
 }
